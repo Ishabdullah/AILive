808a9682820bbae438525ca703cc65fafe41978b
Ismail Abdullah <ismail.t.abdullah@gmail.com>
2025-10-28 07:16:02 -0400

    Ultra-minimal logging test - log to all levels

--- app/src/main/java/com/ailive/MainActivity.kt
+++ app/src/main/java/com/ailive/MainActivity.kt
@@ -3,50 +3,24 @@ package com.ailive
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.ailive.testing.TestScenarios
-import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
-    private val TAG = "MainActivity"
-    private lateinit var ailiveCore: AILiveCore
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.i(TAG, "=== Step 1: onCreate started ===")
+        
+        // Ultra-minimal test - just log to EVERY log level
+        Log.wtf("AILIVE_TEST", "========================================")
+        Log.e("AILIVE_TEST", "ERROR: MainActivity onCreate STARTED")
+        Log.w("AILIVE_TEST", "WARN: MainActivity onCreate STARTED")
+        Log.i("AILIVE_TEST", "INFO: MainActivity onCreate STARTED")
+        Log.d("AILIVE_TEST", "DEBUG: MainActivity onCreate STARTED")
+        Log.v("AILIVE_TEST", "VERBOSE: MainActivity onCreate STARTED")
+        System.out.println("AILIVE_TEST: System.out.println test")
+        System.err.println("AILIVE_TEST: System.err.println test")
+        Log.wtf("AILIVE_TEST", "========================================")
         
         setContentView(R.layout.activity_main)
-        Log.i(TAG, "=== Step 2: setContentView complete ===")
-
-        Log.i(TAG, "=== Step 3: Creating AILiveCore instance ===")
-        try {
-            ailiveCore = AILiveCore(applicationContext, this)
-            Log.i(TAG, "=== Step 4: AILiveCore instance created ===")
-            
-            ailiveCore.initialize()
-            Log.i(TAG, "=== Step 5: AILiveCore initialized ===")
-            
-            ailiveCore.start()
-            Log.i(TAG, "=== Step 6: AILiveCore started ===")
-
-            // Run tests
-            CoroutineScope(Dispatchers.Main).launch {
-                Log.i(TAG, "=== Step 7: Starting test scenarios ===")
-                delay(1000)
-                val tests = TestScenarios(ailiveCore)
-                tests.runAllTests()
-                Log.i(TAG, "=== Step 8: Test scenarios complete ===")
-            }
-        } catch (e: Exception) {
-            Log.e(TAG, "=== CRASH: ${e.message} ===", e)
-            e.printStackTrace()
-        }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.i(TAG, "=== onDestroy called ===")
-        if (::ailiveCore.isInitialized) {
-            ailiveCore.stop()
-        }
+        
+        Log.e("AILIVE_TEST", "UI SET - APP RUNNING")
     }
 }
